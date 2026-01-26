5bf7be6e7e8d4c544d5fb28ffcd3859f2579ad6d
alex289 <mccr4fthd@gmail.com>
2026-01-26 16:37:11 +0100

    feat: Add logo

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -32,16 +32,77 @@ export const Route = createRootRoute({
 		meta: [
 			{
 				name: "description",
-				content: "My App is a web application",
+				content: "GitOps for Docker",
 			},
 			{
 				title: "OrcaCD",
+				charset: "utf-8",
+				viewport: "width=device-width, initial-scale=1",
+				themeColor: "#0f172a",
 			},
 		],
 		links: [
 			{
 				rel: "icon",
 				href: "/assets/favicon.ico",
+			},
+			{
+				rel: "apple-touch-icon",
+				href: "/assets/apple-touch-icon.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/assets/logo-dark-32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "64x64",
+				href: "/assets/logo-dark-64.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "96x96",
+				href: "/assets/logo-dark-96.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "128x128",
+				href: "/assets/logo-dark-128.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "144x144",
+				href: "/assets/logo-dark-144.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				href: "/assets/logo-dark-192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "256x256",
+				href: "/assets/logo-dark-256.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "512x512",
+				href: "/assets/logo-dark-512.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "1024x1024",
+				href: "/assets/logo-dark-1024.png",
 			},
 		],
 	}),
