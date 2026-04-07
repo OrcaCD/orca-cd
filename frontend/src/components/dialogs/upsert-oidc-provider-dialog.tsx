46ace5ba750e479d77e94b8ff904ca41ebf02e7a
Timo Kössler <info@timokoessler.de>
2026-04-07 17:19:52 +0200

    feat: Improve nav and fix mobile menu (#66)

--- frontend/src/components/dialogs/upsert-oidc-provider-dialog.tsx
+++ frontend/src/components/dialogs/upsert-oidc-provider-dialog.tsx
@@ -114,7 +114,7 @@ export default function UpsertOIDCProviderDialog({
 					</Button>
 				) : (
 					<Button>
-						<Plus className="mr-2 h-4 w-4" />
+						<Plus className="h-4 w-4" />
 						Add Provider
 					</Button>
 				)}
