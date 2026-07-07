c80a81216ad8207f8bb58a82dc4f17117b87f35a
Timo Kössler <info@timokoessler.de>
2026-07-07 14:26:41 +0200

    fix: Allow inline wasm loading (#218)

--- backend/internal/hub/middleware/security.go
+++ backend/internal/hub/middleware/security.go
@@ -8,7 +8,7 @@ func SecurityHeaders() gin.HandlerFunc {
 		c.Header("X-Content-Type-Options", "nosniff")
 		c.Header("Referrer-Policy", "same-origin")
 		c.Header("X-Robots-Tag", "noindex, nofollow, noarchive")
-		c.Header("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-ancestors 'none'; base-uri 'self'; object-src 'none'")
+		c.Header("Content-Security-Policy", "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-ancestors 'none'; base-uri 'self'; object-src 'none'")
 		c.Header("Cross-Origin-Opener-Policy", "same-origin")
 		c.Header("Cross-Origin-Resource-Policy", "same-origin")
 		c.Header("Cross-Origin-Embedder-Policy", "require-corp")
