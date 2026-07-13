c59764eff62172a8f520f51615d55ffc676962f8
alex289 <mccr4fthd@gmail.com>
2026-07-13 15:27:39 +0200

    fix: Table head spacing

--- frontend/src/components/tables/application-events/data-table.tsx
+++ frontend/src/components/tables/application-events/data-table.tsx
@@ -40,7 +40,7 @@ export function ApplicationEventsDataTable({ columns, data }: ApplicationEventsD
 					{table.getHeaderGroups().map((headerGroup) => (
 						<TableRow key={headerGroup.id}>
 							{headerGroup.headers.map((header) => (
-								<TableHead key={header.id}>
+								<TableHead key={header.id} className="px-4">
 									{header.isPlaceholder
 										? null
 										: flexRender(header.column.columnDef.header, header.getContext())}
